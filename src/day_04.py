275de0e80182488bbf15b4c701ee24a85ce413ff
Marco Marinho <marcoobom@gmail.com>
2023-12-04 19:35:02 +0100

    Day 04

--- src/day_04.py
+++ src/day_04.py
@@ -3,15 +3,15 @@ from util import get_data
 
 
 def get_intersection(data: list[str]):
-    split = [[part[0], part[1]] for line in data
+    def _get_intersection(first: str, second: str):
+        win = {int(entry) for entry in re.sub(" +", " ", first.strip()).split(" ")}
+        mine = {int(entry) for entry in re.sub(" +", " ", second.strip()).split(" ")}
+        return len(win.intersection(mine))
+
+    split = [_get_intersection(*part) for line in data
              for entry in [line.split(":")]
              for part in [entry[1].split("|")]]
-    intersection = [0] * len(split)
-    for idx, entry in enumerate(split):
-        win = {int(entry) for entry in re.sub(" +", " ", entry[0].strip()).split(" ")}
-        mine = {int(entry) for entry in re.sub(" +", " ", entry[1].strip()).split(" ")}
-        intersection[idx] = len(win.intersection(mine))
-    return intersection
+    return split
 
 
 def task_01(intersection: list[int]):
@@ -25,16 +25,18 @@ def task_01(intersection: list[int]):
 def task_02(intersection: list[int]):
     copies = [1] * len(intersection)
     for idx, value in enumerate(copies):
-        if value == 0:
-            break
         for offset in range(1, intersection[idx] + 1):
             copies[idx + offset] += 1 * value
     return sum(copies)
 
 
-if __name__ == "__main__":
+def main():
     data = get_data("../data/Day04.txt")
     intersection = get_intersection(data)
 
     print("Task 01: ", task_01(intersection))
     print("Task 02: ", task_02(intersection))
+
+
+if __name__ == "__main__":
+    main()

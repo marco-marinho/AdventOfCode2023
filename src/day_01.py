0a3a821a73f0c1830381f4d99d455c9145f63a6d
Marco Marinho <marcoobom@gmail.com>
2024-06-21 20:43:48 +0200

    Day 24

--- src/day_01.py
+++ src/day_01.py
@@ -3,28 +3,17 @@ import re
 from util import get_data
 
 
-def str_to_num(istr: str) -> int:
-    match istr:
-        case "1" | "one":
-            return 1
-        case "2" | "two":
-            return 2
-        case "3" | "three":
-            return 3
-        case "4" | "four":
-            return 4
-        case "5" | "five":
-            return 5
-        case "6" | "six":
-            return 6
-        case "7" | "seven":
-            return 7
-        case "8" | "eight":
-            return 8
-        case "9" | "nine":
-            return 9
-        case _:
-            raise ValueError("Invalid Input")
+str_num_mapping = {
+    '1': 1, 'one': 1,
+    '2': 2, 'two': 2,
+    '3': 3, 'three': 3,
+    '4': 4, 'four': 4,
+    '5': 5, 'five': 5,
+    '6': 6, 'six': 6,
+    '7': 7, 'seven': 7,
+    '8': 8, 'eight': 8,
+    '9': 9, 'nine': 9,
+}
 
 
 def task1(data: list[str]) -> int:
@@ -36,7 +25,7 @@ def task1(data: list[str]) -> int:
 def task2(data: list[str]) -> int:
     pattern = r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
     tokens = [re.findall(pattern, entry) for entry in data]
-    numbers = [str_to_num(entry[0]) * 10 + str_to_num(entry[-1]) for entry in tokens]
+    numbers = [str_num_mapping[entry[0]] * 10 + str_num_mapping[entry[-1]] for entry in tokens]
     return sum(numbers)
 
 
---
+++ src/day_24.py
@@ -0,0 +1,53 @@
+import numpy as np
+from numpy.typing import NDArray
+from itertools import combinations
+
+from util import get_data
+
+cross = lambda x, y: np.cross(x, y)
+
+
+def task_01(data: list[tuple[NDArray[np.float64], NDArray[np.float64]]]):
+    count = 0
+    minx = miny = 200000000000000
+    maxx = maxy = 400000000000000
+    for first, second in combinations(data, 2):
+        p = first[0][:2]
+        r = first[1][:2]
+        q = second[0][:2]
+        s = second[1][:2]
+        a = q - p
+        b = cross(r, s)
+        if b == 0:
+            continue
+        t = cross(a, s / b)
+        u = cross(a, r / b)
+        if t < 0 or u < 0:
+            continue
+        intersection = p + t * r
+        if minx <= intersection[0] <= maxx and miny <= intersection[1] <= maxy:
+            count += 1
+    return count
+
+
+def task_02(data: list[tuple[NDArray[np.float64], NDArray[np.float64]]]):
+    p = data[1][0] - data[0][0]
+    r = data[1][1] - data[0][1]
+    q = data[2][0] - data[0][0]
+    s = data[2][1] - data[0][1]
+    t1 = -(cross(p, q) @ s) / (cross(r, q) @ s)
+    t2 = -(cross(p, q) @ r) / (cross(p, s) @ r)
+    c1 = data[1][0] + t1 * data[1][1]
+    c2 = data[2][0] + t2 * data[2][1]
+    v = (c2 - c1) / (t2 - t1)
+    ans = c1 - t1 * v
+    return int(np.sum(ans))
+
+
+if __name__ == "__main__":
+    lines = get_data("../data/Day24.txt")
+    data = [(np.fromstring(first, sep=",", dtype=np.float64), np.fromstring(second, sep=",", dtype=np.float64))
+            for line in lines
+            for first, second in [line.split("@")]]
+    print("Task 01:", task_01(data))
+    print("Task 02:", task_02(data))

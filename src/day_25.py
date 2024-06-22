4da72a47e9c1b7d21f59ed649931ec3049067ab3
Marco Marinho <marcoobom@gmail.com>
2024-06-22 10:31:35 +0200

    Day 25

---
+++ src/day_25.py
@@ -0,0 +1,24 @@
+from util import get_data
+from itertools import chain
+
+import numpy as np
+
+lines = get_data('../data/Day25.txt')
+data = [line.replace(":", "").split(" ") for line in lines]
+indexes = {name: index for index, name in enumerate(set(chain(*data)))}
+
+laplacian = np.zeros((len(indexes), len(indexes)))
+for line in data:
+    head = line[0]
+    for tail in line[1:]:
+        laplacian[indexes[head], indexes[tail]] = -1
+        laplacian[indexes[tail], indexes[head]] = -1
+
+for column in range(laplacian.shape[1]):
+    laplacian[column, column] = -np.sum(laplacian[:, column])
+
+vals, vecs = np.linalg.eigh(laplacian)
+sorted_vecs = vecs[:, vals.argsort()]
+
+ans = sum(sorted_vecs[:, 1] > 0) * sum(sorted_vecs[:, 1] < 0)
+print("Task 01:", ans)
